846b9b4d15edb54bd25cf242b26c054020481c60
omar hernandez <omher1105@gmail.com>
2018-09-02 23:29:17 -0500

    modificando el modelo de usuario

--- migrations/20161215104944_user.js
+++ migrations/20161215104944_user.js
@@ -1,14 +1,16 @@
-
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('user', table => {
-    table.increments();
-    table.text('email').unique().notNullable();
-    table.text('password').notNullable();
-    table.datetime('date').notNullable();
-    table.boolean('is_active').notNullable().defaultTo(true);
-  });
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('user', table => {
+        table.increments();
+        table.text('email').unique().notNullable();
+        table.text('password').notNullable();
+        table.text('name').notNullable();
+        table.text('lastname').notNullable();
+        table.datetime('date').notNullable();
+        table.datetime('updated_at').notNullable();
+        table.boolean('is_active').notNullable().defaultTo(true);
+    });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('user');
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('user');
 };
